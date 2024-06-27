4efe5d2f681b6f0e01da956db8e182aa9f63801b
calamoni <95957245+calamoni@users.noreply.github.com>
2024-06-27 16:52:46 -0700

    Update VulnerableAppApplication.java

--- src/main/java/fr/christophetd/log4shell/vulnerableapp/VulnerableAppApplication.java
+++ src/main/java/fr/christophetd/log4shell/vulnerableapp/VulnerableAppApplication.java
@@ -2,12 +2,21 @@ package fr.christophetd.log4shell.vulnerableapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VulnerableAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VulnerableAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VulnerableAppApplication.class, args);
+    }
 
+    @Bean
+    public FilterRegistrationBean<CustomHeaderFilter> customHeaderFilter() {
+        FilterRegistrationBean<CustomHeaderFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new CustomHeaderFilter());
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
+    }
 }
